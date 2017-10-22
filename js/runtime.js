45b440ea0b5cd0191b9b6508c45356b833e0f5ca
ProfInLine <altahackcommunity@gmail.com>
2017-10-22 02:58:06 +0200

    Opti heart

--- js/runtime.js
+++ js/runtime.js
@@ -1,4 +1,12 @@
 (function () {
+	function loadCss(filename, filetype)
+	{
+		let fileref = document.createElement("link");
+		fileref.rel = "stylesheet";
+		fileref.type = "text/css";
+		fileref.href = filename;
+		document.getElementsByTagName("head")[0].appendChild(fileref)
+	}
 	function loadScript(url, callback) {
 		var script = document.createElement("script")
 		script.type = "text/javascript";
@@ -17,6 +25,9 @@
 		script.src = url;
 		document.getElementsByTagName("head")[0].appendChild(script);
 	}
+	loadCss("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+	loadCss("css/bootstrap.min.css");
+	loadCss("css/style.css");
 	loadScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js", function () {
 		loadScript("js/bootstrap.min.js", function () {
 			loadScript("js/peer.min.js", function () {
