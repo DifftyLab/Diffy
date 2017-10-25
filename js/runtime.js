e2c8d126ff02b7821b450f02cc6a913e5e20f93a
aabb <twitchfordoog@gmail.com>
2017-10-25 03:09:58 +0200

    Micro heart

--- js/runtime.js
+++ js/runtime.js
@@ -88,8 +88,31 @@ var connection = null;
 						connection.onmessage = function(event) {
 							alert(event.userid + ' said: ' + event.data);
 						};
-						loadScript("js/tilt.jquery.js", function(){});
-						loadScript("js/bootbox.min.js", function(){});
+						loadScript("js/tilt.jquery.js", function(){
+							$('.tilt-poster').tilt({
+								scale: 1.05,
+								perspective: 500
+							});
+						});
+						loadScript("js/bootbox.min.js", function(){
+							$("#choosemovie").click(function(){
+    bootbox.confirm({
+        title: "Destroy planet?",
+        message: "Do you want to activate the Deathstar now? This cannot be undone.",
+        buttons: {
+            cancel: {
+                label: '<i class="fa fa-times"></i> Cancel'
+            },
+            confirm: {
+                label: '<i class="fa fa-check"></i> Confirm'
+            }
+        },
+        callback: function (result) {
+            console.log('This was logged in the callback: ' + result);
+        }
+    });
+							});
+						});
 					});
 				});
 			});	
