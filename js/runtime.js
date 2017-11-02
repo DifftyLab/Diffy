ea856c9760db0abd4cd6b3972b3fdbe815eea635
aabb <twitchfordoog@gmail.com>
2017-11-02 10:33:00 +0100

    Micro heart

--- js/runtime.js
+++ js/runtime.js
@@ -42,9 +42,9 @@ var player = null;
 		gtag('config', 'UA-108834973-1');
 	});
 	loadScript("js/jquery-3.2.1.min.js", function () {
-		loadScript("js/jquery.mobile-1.4.5.min.js", function () {
-			loadScript("js/popper.min.js", function () {
-				loadScript("js/bootstrap.min.js", function () {
+		loadScript("js/popper.min.js", function () {
+			loadScript("js/bootstrap.min.js", function () {
+				loadScript("js/jquery.mobile-1.4.5.min.js", function () {
 					loadScript("https://diffyheart.herokuapp.com:443/socket.io/socket.io.js", function () {
 						loadScript("https://diffyheart.herokuapp.com:443/dist/RTCMultiConnection.js", function () {
 							loadScript("js/clientjs.min.js", function(){
